04bcba25a22e94b0ef12cb245edca30300a789f2
Lorenzo Figueroa Wlack <lorenzo.f.w12@gmail.com>
2024-07-02 19:37:45 -0400

    Actualizada la clase Banco para cargar clientes y cuentas desde un archivo (metodo de "leerClientesDesdArchivo" aún no creado.

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -1,16 +1,32 @@
 package banco;
 
+import banco.entidades.Cliente;
+import banco.transacciones.FileManager;
 import ui.Login;
 
-public class Banco{
+import java.io.IOException;
+import java.util.List;
+
+public class Banco {
+  private List<Cliente> clientes;
+
   public Banco() {
     // Constructor
   }
-  public static void main(String[] args) {
-    // instancia de la clase Login
-    Login login = new Login();
-    login.setVisible(true);
 
-    // Simular instancias de cliente para usar la interfaz gráfica
+  public static void main(String[] args) {
+    Banco banco = new Banco();
+    banco.cargarClientes();
+    // Instancia de la clase Login
+    Login login = new Login(banco.clientes);
+    login.setVisible(true);
+  }
+
+  private void cargarClientes() {
+    try {
+      clientes = FileManager.leerClientesDesdeArchivo("transacciones");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
