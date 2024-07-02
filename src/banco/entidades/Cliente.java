b4628c5d0858d18b421fa8115d1bbd19cf054b55
Lorenzo Figueroa Wlack <lorenzo.f.w12@gmail.com>
2024-07-02 19:27:56 -0400

    Implementadas clases Cuenta y Transaccion en el paquete entidades. Actualizada la clase Cliente para manejar cuentas y transacciones.

--- src/banco/entidades/Cliente.java
+++ src/banco/entidades/Cliente.java
@@ -1,14 +1,71 @@
 package banco.entidades;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cliente {
   private String username;
   private String password;
-
   private String nombre;
   private String edad;
   private String rut;
+  private List<Cuenta> cuentas;
 
-  private List<String> cuentas;
+  public Cliente(String username, String password, String nombre, String edad, String rut) {
+    this.username = username;
+    this.password = password;
+    this.nombre = nombre;
+    this.edad = edad;
+    this.rut = rut;
+    this.cuentas = new ArrayList<>();
+  }
+
+  // consrtuctor
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getEdad() {
+    return edad;
+  }
+
+  public void setEdad(String edad) {
+    this.edad = edad;
+  }
+
+  public String getRut() {
+    return rut;
+  }
+
+  public void setRut(String rut) {
+    this.rut = rut;
+  }
+
+  public List<Cuenta> getCuentas() {
+    return cuentas;
+  }
+
+  public void agregarCuenta(Cuenta cuenta) {
+    this.cuentas.add(cuenta);
+  }
 }
--- src/banco/entidades/Transaccion.java
+++ src/banco/entidades/Transaccion.java
@@ -1,4 +1,51 @@
 package banco.entidades;
 
+import java.time.LocalDateTime;
+
 public class Transaccion {
+    private String tipo;
+    private double monto;
+    private LocalDateTime fechaTransaccion;
+    private String cliente;
+
+    public Transaccion(String tipo, double monto, LocalDateTime fechaTransaccion, String cliente) {
+        this.tipo = tipo;
+        this.monto = monto;
+        this.fechaTransaccion = fechaTransaccion;
+        this.cliente = cliente;
+    }
+
+    // bob construye
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public LocalDateTime getFechaTransaccion() {
+        return fechaTransaccion;
+    }
+
+    public void setFechaTransaccion(LocalDateTime fechaTransaccion) {
+        this.fechaTransaccion = fechaTransaccion;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
 }
